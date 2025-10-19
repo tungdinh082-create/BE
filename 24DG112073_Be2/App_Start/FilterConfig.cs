00937054913df652a182daa5cab5acad99415f49
tungdinh082-create <tungdinh082@gmail.com>
2025-10-20 02:12:31 +0700

    Initial commit

---
+++ 24DG112073_Be2/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _24DG112073_Be2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
+
---
+++ 24DG112073_Be2/Global.asax
@@ -0,0 +1,2 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24DG112073_Be2.MvcApplication" Language="C#" %>
+
